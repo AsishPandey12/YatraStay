3e49809103feaee1e0a8a409d02f116213baf8c5
AsishPandey12 <asishpandey9163@gmail.com>
2025-05-02 23:31:25 +0530

    add styling to the code base

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const mongoose = require("mongoose");
 const Listing = require("./models/listing");
 const path = require("path");
 const methodoverride = require("method-override");
+const ejsMate = require("ejs-mate");
 
 const Mongo_URL = "mongodb://127.0.0.1:27017/YatraStay";
 
@@ -21,6 +22,8 @@ app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.urlencoded({ extended: true }));
 app.use(methodoverride("_method"));
+app.engine("ejs", ejsMate);
+app.use(express.static(path.join(__dirname, "/public")));
 
 app.get("/", (req, res) => {
   res.send("Root is working");
@@ -55,7 +58,6 @@ app.post("/listings", async (req, res) => {
 app.get("/listings/:id/edit", async (req, res) => {
   let { id } = req.params;
   let listing = await Listing.findById(id);
-  console.log(listing);
   res.render("listings/edit.ejs", { listing });
 });
 
--- models/listing.js
+++ models/listing.js
@@ -10,10 +10,10 @@ const listingSchema = new Schema({
   image: {
     type: String,
     default:
-      "https://unsplash.com/photos/person-in-swimming-pool-during-daytime-rlwE8f8anOc",
+      "https://images.unsplash.com/photo-1551882547-ff40c63fe5fa?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     set: (v) =>
       v === ""
-        ? "https://unsplash.com/photos/person-in-swimming-pool-during-daytime-rlwE8f8anOc"
+        ? "https://images.unsplash.com/photo-1551882547-ff40c63fe5fa?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
         : v,
   },
   price: Number,
