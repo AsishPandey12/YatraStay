7c06fd53e770e815d841b99996d7a1f8704a979e
AsishPandey12 <asishpandey9163@gmail.com>
2025-05-06 23:40:41 +0530

    add validation for schema using joi.

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const methodoverride = require("method-override");
 const ejsMate = require("ejs-mate");
 const ExpressError = require("./utils/ExpressError.js");
 const wrapAsync = require("./utils/wrapAsync.js");
+const { listingSchema } = require("./schema.js");
 
 // MongoDb connection
 const Mongo_URL = "mongodb://127.0.0.1:27017/YatraStay";
@@ -61,10 +62,11 @@ app.get(
 app.post(
   "/listings",
   wrapAsync(async (req, res) => {
-    if (!req.body.listing) {
-      throw new ExpressError(400, "Send valid data for listing");
-    }
+    let result = listingSchema.validate(req.body);
     const newListing = new Listing(req.body.listing);
+    if (result.error) {
+      throw new ExpressError(400, result.error);
+    }
     await newListing.save();
     res.redirect("/listings");
   })
