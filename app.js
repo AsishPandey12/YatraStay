5dad9ce02fd412922c80074af7e49a35357dceb3
AsishPandey12 <asishpandey9163@gmail.com>
2025-05-07 00:10:14 +0530

    defining validate listing middleware function

--- app.js
+++ app.js
@@ -34,6 +34,17 @@ app.get("/", (req, res) => {
   res.send("Root is working");
 });
 
+// validation for schema - middleware function
+const validateListing = (req, res, next) => {
+  let { error } = listingSchema.validate(req.body);
+  if (error) {
+    let errMsg = error.details.map((el) => el.message).join(",");
+    throw new ExpressError(400, errMsg);
+  } else {
+    next();
+  }
+};
+
 // Index Route
 app.get(
   "/listings",
@@ -61,12 +72,9 @@ app.get(
 // Create Route
 app.post(
   "/listings",
+  validateListing,
   wrapAsync(async (req, res) => {
-    let result = listingSchema.validate(req.body);
     const newListing = new Listing(req.body.listing);
-    if (result.error) {
-      throw new ExpressError(400, result.error);
-    }
     await newListing.save();
     res.redirect("/listings");
   })
@@ -85,10 +93,8 @@ app.get(
 // Update Route
 app.put(
   "/listings/:id",
+  validateListing,
   wrapAsync(async (req, res) => {
-    if (!req.body.listing) {
-      throw new ExpressError(400, "Send valid data for listing");
-    }
     let { id } = req.params;
     await Listing.findByIdAndUpdate(id, {
       ...req.body.listing,
