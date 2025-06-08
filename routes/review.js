c8c03ee0300b0f34e041a0df4b436b98470c5372
AsishPandey12 <asishpandey9163@gmail.com>
2025-06-08 20:19:27 +0530

    implemented authentication and authorization in the project

--- routes/review.js
+++ routes/review.js
@@ -1,29 +1,23 @@
 const express = require("express");
 const router = express.Router({ mergeParams: true });
-const ExpressError = require("../utils/ExpressError.js");
 const wrapAsync = require("../utils/wrapAsync.js");
-const { reviewSchema } = require("../schema.js");
 const Review = require("../models/review.js");
 const Listing = require("../models/listing");
-
-// validation for schema - middleware function
-const validateReview = (req, res, next) => {
-  let { error } = reviewSchema.validate(req.body);
-  if (error) {
-    let errMsg = error.details.map((el) => el.message).join(",");
-    throw new ExpressError(400, errMsg);
-  } else {
-    next();
-  }
-};
+const {
+  validateReview,
+  isLoggedIn,
+  isReviewAuthor,
+} = require("../middleware.js");
 
 // Post Reviews Route
 router.post(
   "/",
+  isLoggedIn,
   validateReview,
   wrapAsync(async (req, res) => {
     let listing = await Listing.findById(req.params.id);
     let newReview = await Review(req.body.review);
+    newReview.author = req.user._id;
 
     listing.reviews.push(newReview);
 
@@ -39,12 +33,14 @@ router.post(
 
 // Delete Reviews Route
 router.delete(
-  "/:reviewID",
+  "/:reviewId",
+  isLoggedIn,
+  isReviewAuthor,
   wrapAsync(async (req, res) => {
-    let { id, reviewID } = req.params;
+    let { id, reviewId } = req.params;
 
-    await Listing.findByIdAndUpdate(id, { $pull: { reviews: reviewID } });
-    await Review.findByIdAndDelete(reviewID);
+    await Listing.findByIdAndUpdate(id, { $pull: { reviews: reviewId } });
+    await Review.findByIdAndDelete(reviewId);
 
     req.flash("success", "Review Deleted!");
 
